40b5219fa5e3699175432f7ce2798c6f2287ded1
chaaarlesei <charlesignacio6@gmail.com>
2025-06-17 10:57:44 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, send_file, render_template
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/remove-bg', methods=['POST'])
+def remove_bg():
+    if 'image' not in request.files:
+        return 'No image uploaded', 400
+
+    image_file = request.files['image']
+    input_image = Image.open(image_file.stream)
+    output_image = remove(input_image)
+
+    byte_io = io.BytesIO()
+    output_image.save(byte_io, 'PNG')
+    byte_io.seek(0)
+
+    return send_file(byte_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
